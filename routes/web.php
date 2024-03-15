f7039483242f034f0d4f40b4191e6c342b4f260a
bayurahmat76 <163236397+bayurahmat76@users.noreply.github.com>
2024-03-16 00:06:02 +0700

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,29 @@
 <?php
 
+use App\Http\Controllers\siswacontroller;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/coba',function(){
+
+Route::get('hello/{nama}/{nama2}', function () {
+    return 'haloo';
+});
+
+Route::get('coba', function () {
     return view('coba');
 });
 
-Route::get('templete',function(){
-    return view('templete');
+Route::get('admin', function () {
+    return view('template');
 });
 
-Route::get('tabel',function(){
+Route::get('table', function () {
     return view('tabel');
 });
-Route::post('users/{id}',function($id){});
-Route::put('users/{id}',function($id){});
-Route::delete('users/{id}',function($id){});
+Route::resource('siswa', SiswaController::class)->middleware(['auth','admin']);
+ 
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
